3437bc1b95914f2bf576d828163cee0e1cb302b1
Matt Cavalier <matthewjcavalier@gmail.com>
2018-01-16 15:52:33 -0600

    the dungeon is bieng populated with tiles that are either a border or they have a hardness value

--- assignments/dungeonCrawler/crawler.c
+++ assignments/dungeonCrawler/crawler.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX_DUNGEON_HEIGHT 21
 #define MAX_DUNGEON_WIDTH 80
 #define MAX_ROCK_HARDNESS 5
 #define IMMUTABLE -1
 #define OPEN_SPACE 0
+#define BORDER_HOR_CHAR '-'
+#define BORDER_VERT_CHAR '|'
+#define ROOM_CHAR '.'
+#define HALL_CHAR '#'
 
 typedef enum {false, true} boolean;
 
@@ -22,11 +27,15 @@ typedef struct {
 
 Dungeon* genDungeon();
 
+void addBorders(Dungeon* dun);
+
 void setHardnesses(Dungeon* dun);
 
 void placeRooms(Dungeon* dun);
 
 void placeHallways(Dungeon* dun);
+
+void printDungeon(Dungeon* dun);
 
 int main(int argc, char* argv[]) {
   // set up the dungeon
@@ -34,13 +43,15 @@ int main(int argc, char* argv[]) {
   dungeon = genDungeon();
   
   // print the dungeon
-  
+  printDungeon(dungeon);
 }
 
 Dungeon* genDungeon() {
   Dungeon* newDungeon;
   newDungeon = malloc(sizeof(Dungeon));
   
+  addBorders(newDungeon);
+
   setHardnesses(newDungeon);
 
   placeRooms(newDungeon);
@@ -50,10 +61,29 @@ Dungeon* genDungeon() {
   return newDungeon;
 }
 
+void addBorders(Dungeon* dun) {
+  int row, col;
+
+  // add borders to the first row and last row
+  for(col = 0; col < MAX_DUNGEON_WIDTH; col++) {
+    dun->map[0][col].isBorder = true;
+    dun->map[MAX_DUNGEON_HEIGHT - 1][col].isBorder = true;
+  }
+  
+  // add borders to the first and last column
+  for(row = 0; row < MAX_DUNGEON_HEIGHT; row++) {
+    dun->map[row][0].isBorder = true;
+    dun->map[row][MAX_DUNGEON_WIDTH - 1].isBorder = true;
+  }
+}
+
 void setHardnesses(Dungeon* dun) {
   int row, col;
+
+  srand(time(0));
   for(row = 1; row < MAX_DUNGEON_HEIGHT - 1; row++) {
       for(col = 1; col < MAX_DUNGEON_WIDTH - 1; col ++) {
+        dun->map[row][col].hardness = rand() % MAX_ROCK_HARDNESS + 1;
       }
   }
 }
@@ -66,3 +96,26 @@ void placeHallways(Dungeon* dun) {
 
 }
 
+void printDungeon(Dungeon* dun) {
+  int row, col;
+  for(row = 0; row < MAX_DUNGEON_HEIGHT; row++) {
+    for(col = 0; col < MAX_DUNGEON_WIDTH; col++) {
+      // if the tile is a border
+      if(dun->map[row][col].isBorder) {
+        // if the cell is in the first or last row
+        if(row == 0 || row == MAX_DUNGEON_HEIGHT - 1) {
+          printf("%c", BORDER_HOR_CHAR);
+        }
+        // else the cell is one either in the first or last col
+        else {
+          printf("%c", BORDER_VERT_CHAR);
+        }
+      }
+      // else the tile is part of the normal dungeon
+      else {
+        printf("%d", dun->map[row][col].hardness);
+      }
+    }
+    printf("\n");
+  }
+}
